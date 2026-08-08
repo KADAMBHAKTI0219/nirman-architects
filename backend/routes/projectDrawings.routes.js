fe90e6a7b9fb8800579d09d1cac085f5b9afd15c
KADAMBHAKTI0219 <kadambhakti0219@gmail.com>
2026-08-08 16:17:11 +0530

    drawing category

--- backend/routes/projectDrawings.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const drawingController = require('../controllers/drawing.controller');
-
-// 25.11 GET /api/projects/:projectId/drawings/breakdown
-router.get('/:projectId/drawings/breakdown', drawingController.getProjectDrawingsBreakdown);
-
-module.exports = router;
